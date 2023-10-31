d1c9bbdd7ab134c453ffe9f82721f51d2923af5c
el hakik amina <amina.el.hakik2@gmail.com>
2023-10-31 14:00:25 -0700

    task 102 alx

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def magic_calculation(a,b,c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
